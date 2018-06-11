afa952c7bc3c290b781437c59f2f485b9f625e6c
Karim Oulad Chalha <k.ouladchalha@graviton.ma>
2018-06-11 12:42:10 +0000

    BUGFIX: test

--- src/app/shared/components/tiers-form/tiers-form.component.ts
+++ src/app/shared/components/tiers-form/tiers-form.component.ts
@@ -46,19 +46,22 @@ export class TiersFormComponent implements OnInit {
       this.education_degree = {
         dataSource: Helper.dataSourceformatter(this.vars['education_degree']),
         displayExpr: 'Name',
-        valueExpr: 'ID'
+        valueExpr: 'ID',
+        value: Helper.dataSourceformatter(this.vars['education_degree'])[0].ID
       };
 
       this.civil_status = {
         dataSource: Helper.dataSourceformatter(this.vars['civil_status']),
         displayExpr: 'Name',
-        valueExpr: 'ID'
+        valueExpr: 'ID',
+        value: Helper.dataSourceformatter(this.vars['civil_status'])[0].ID
       };
 
       this.sexe = {
         dataSource: Helper.dataSourceformatter(this.vars['sexe']),
         displayExpr: 'Name',
-        valueExpr: 'ID'
+        valueExpr: 'ID',
+        value: Helper.dataSourceformatter(this.vars['sexe'])[0].ID
       };
       this.payment_method = {
         dataSource: Helper.dataSourceformatter(this.vars['payment_method']),
