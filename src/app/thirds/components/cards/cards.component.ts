1ef60e6b5d10b55199ea31a5412c79af59222450
Karim Oulad Chalha <k.ouladchalha@graviton.ma>
2018-06-17 22:33:02 +0100

    GENERAL: ...

--- src/app/thirds/components/cards/cards.component.ts
+++ src/app/thirds/components/cards/cards.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { CardsService } from '../../../contracts/services/cards.service';
 import { Card } from 'primeng/card';
+import {ThirdsService} from '../../services/thirds.service';
 
 @Component({
   selector: 'app-cards',
@@ -14,22 +15,31 @@ export class CardsComponent implements OnInit {
   applyFilter: any;
   action: string;
   selectedItems: any[];
+  card_status: any;
 
   constructor(private toastr: ToastrService,
-    private cardsService: CardsService) { }
+    private cardsService: CardsService,
+              public thirdService: ThirdsService) { }
 
   ngOnInit() {
-    this.cardsService.getCards().subscribe(cards => {
-      this.cards = this.cardsService.dataFormatter(cards, false);
-      this.cards = this.cards.map(card => {
-        card.full_name = `${card.first_name} ${card.last_name}`;
-        return card;
-      });
-    },
-      (err) => {
-        throw err;
-        // this.toastr.error(err.error.message);
-      });
+    this.thirdService.getThirdsVars().subscribe(data => {
+      console.log(data);
+      this.card_status = data['card_status'];
+      this.cardsService.getCards().subscribe(cards => {
+          this.cards = this.cardsService.dataFormatter(cards, false);
+          this.cards = this.cards.map(card => {
+            card.full_name = `${card.first_name} ${card.last_name}`;
+            card.status = this.card_status[card.status];
+            return card;
+          });
+        },
+        (err) => {
+          throw err;
+          // this.toastr.error(err.error.message);
+        });
+    }, error1 => {
+      throw error1;
+    });
   }
 
   onStartEdit = (e) => {
