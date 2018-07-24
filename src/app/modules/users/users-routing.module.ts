d3929cfddb06d60568bd3fb881deb135f16e5fab
Lamyae Ennadi <ennadi.lamyae@gmail.com>
2018-07-24 17:10:47 +0100

    FEATUR: creation du dossier list dans le module users

--- src/app/modules/users/users-routing.module.ts
+++ src/app/modules/users/users-routing.module.ts
@@ -2,14 +2,16 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {DefaultComponent} from '../../theme/pages/default/default.component';
 import {AddComponent} from './components/add/add.component';
+import { ListComponent } from './components/list/list.component';
 
 const routes: Routes = [{
   path: 'utilisateurs',
   component: DefaultComponent,
   children: [
-    {path: 'liste', component: AddComponent}
+    {path: 'ajouter', component: AddComponent},
+    {path: 'list', component: ListComponent}
   ]
-}];
+}]; 
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
