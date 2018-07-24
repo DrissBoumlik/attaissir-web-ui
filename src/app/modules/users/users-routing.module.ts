af4cc14109e316d7a6a2904f0c922117380f8de5
mohamed eloudifi <mohammed.eloudifi@gmail.com>
2018-07-24 16:59:16 +0100

    FEATURE: ajoute le lien du userModule dans theme-routing.module.ts

--- src/app/modules/users/users-routing.module.ts
+++ src/app/modules/users/users-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {DefaultComponent} from '../../theme/pages/default/default.component';
+import {AddComponent} from './components/add/add.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'utilisateurs',
+  component: DefaultComponent,
+  children: [
+    {path: 'liste', component: AddComponent}
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class UsersRoutingModule { }
+export class UsersRoutingModule {
+}
