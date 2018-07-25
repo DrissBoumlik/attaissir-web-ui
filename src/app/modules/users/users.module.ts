a05dfba2e7539366a24b18eed774e0f7df8ec184
Karim Oulad Chalha <k.ouladchalha@graviton.ma>
2018-07-25 15:27:52 +0100

    GENERAL: Corriger les errors après merge

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { AddComponent } from './components/add/add.component';
-import { ListComponent } from './list/list.component';
+import {ListComponent} from './components/list/list.component';
+import {SharedModule} from '../../shared/shared.module';
 
 @NgModule({
   imports: [
+    SharedModule,
     CommonModule,
     UsersRoutingModule
   ],
-  declarations: [AddComponent, ListComponent]
+  declarations: [AddComponent, ListComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class UsersModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://s5.kouladchalha.code.go'
+
+};
