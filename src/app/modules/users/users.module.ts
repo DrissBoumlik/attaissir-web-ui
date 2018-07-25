229223f9f89fc6ad1bef6209069453ab2776c9ad
Lamyae Ennadi <ennadi.lamyae@gmail.com>
2018-07-25 15:01:08 +0100

    Utilisateurs 	-  Création d'une page pour lister les utilisateurs (Done)

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { AddComponent } from './components/add/add.component';
-import { ListComponent } from './list/list.component';
+import { ListComponent } from './components/list/list.component';
+import { DxFormModule } from '../../../../node_modules/devextreme-angular';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   imports: [
+    SharedModule,
     CommonModule,
     UsersRoutingModule
   ],
