b9161158f6b9cdb897f04aabb8d0bd3a5edef9c3
Lamyae Ennadi <ennadi.lamyae@gmail.com>
2018-07-26 18:21:45 +0100

    Création d'une page pour lister les interventions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://api.siam618.graviton.ma'
+  // apiUrl: 'http://api.siam618.graviton.ma'
+  apiUrl: 'http://s9.nmouline.code.go',
 
 };
