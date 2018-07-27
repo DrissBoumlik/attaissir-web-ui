2198c182d6c14888f4c002f2a769723f592f2060
Karim Oulad Chalha <k.ouladchalha@graviton.ma>
2018-07-27 17:22:09 +0100

    GENERAL: ...

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://api.siam618.graviton.ma'
-  apiUrl: 'http://s9.nmouline.code.go',
+  apiUrl: 'http://api.siam618.graviton.ma'
 
 };
