28f91195483f1d6912b0a46ed4be90176162bd40
h.hmidani <h.hmidani@graviton.ma>
2018-08-01 18:00:25 +0100

    GENERAL

--- src/app/modules/users/users.module.ts
+++ src/app/modules/users/users.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { AddComponent } from './components/add/add.component';
 import { ListComponent } from './components/list/list.component';
+import { EditComponent } from './components/edit/edit.component';
 import { SharedModule } from '../../shared/shared.module';
 import { DxButtonModule, DxListModule, DxPopupModule, DxSelectBoxModule, DxTemplateModule, DxTreeViewModule } from 'devextreme-angular';
 import { ProfileComponent } from './components/profile/profile.component';
@@ -21,7 +22,7 @@ import {AvatarModule} from 'ng2-avatar';
     DxButtonModule,
     AvatarModule.forRoot()
   ],
-  declarations: [AddComponent, ListComponent, ProfileComponent],
+  declarations: [AddComponent, ListComponent, EditComponent , ProfileComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class UsersModule {
