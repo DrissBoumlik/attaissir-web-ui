2adcd881bec4b904b0e0c73161684e320ea3facb
ZAQOURI Oussama <oussamazaqouri@gmail.com>
2018-08-03 17:55:01 +0100

    modifier les filtres

--- src/app/modules/roles/roles-routing.module.ts
+++ src/app/modules/roles/roles-routing.module.ts
@@ -4,14 +4,13 @@ import { DefaultComponent } from '../../theme/pages/default/default.component';
 import { AddComponent } from './components/add/add.component';
 import { ListComponent } from './components/list/list.component';
 import { EditeComponent } from './components/edite/edite.component';
-import { AuthGuard } from '../../_directives/guard.directive';
 
 const routes: Routes = [{
   path: 'roles',
   component: DefaultComponent,
   children: [
     { path: 'ajouter', component: AddComponent },
-    { path: 'liste', component: ListComponent, canActivate : [AuthGuard] , data: { permission: []} },
+    { path: 'liste', component: ListComponent },
     { path: 'modifier/:id', component: EditeComponent }
   ]
 }];
