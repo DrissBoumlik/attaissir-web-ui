361d0729d60e2b2b417a3a1ccc55a142c1660668
h.hmidani <h.hmidani@graviton.ma>
2018-08-03 11:43:05 +0100

    A

--- src/app/_directives/permission.directive.ts
+++ src/app/_directives/permission.directive.ts
@@ -2,7 +2,7 @@ import {Directive, ElementRef, HostListener, Input, OnInit} from '@angular/core'
 
 
 @Directive({
-  selector: '[permissionHidden]'
+  selector: '[appPermissionHidden]'
 })
 export class PermissionDirective  implements  OnInit {
 
